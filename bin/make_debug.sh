8c8b4f156883d77e11de83d8a8a92bf2b96e4089
Sabir777 <sabir.yanin2014@yandex.ru>
2023-12-22 20:55:10 +0300

    Сборка отладочной и рабочей версии программы в одной директории

---
+++ bin/make_debug.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Передаю в Makefile переменную "compile_flags"
+make compile_flags="-O0 -g"
+
---
+++ bin/make_release.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Передаю в Makefile переменную "compile_flags"
+make compile_flags="-O3 -funroll-loops -fomit-frame-pointer"
+
